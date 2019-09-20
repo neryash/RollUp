d74ab8858c02641e07e0f0b536a7c5c3c1c48b02
nerya <neryasheetirt@gmail.com>
2019-09-20 18:40:34 +0300

    i finished importing the arraylists from the server to the activity, and then from one activity to the other, afterwords, i managed to extract the main array list. //TODO: convert arraylist into the frames of the dance!! and then i am finished!

--- app/src/main/java/com/nerya/rollup/showFiles.java
+++ app/src/main/java/com/nerya/rollup/showFiles.java
@@ -2,12 +2,15 @@ package com.nerya.rollup;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.parse.FindCallback;
 import com.parse.ParseException;
@@ -27,8 +30,10 @@ public class showFiles extends AppCompatActivity implements AdapterView.OnItemCl
     TextView displayText;
     ListView listView;
     ArrayList dances;
-    List<String> myList;
+    ArrayList projectInfo;
     ArrayAdapter arrayAdapter;
+    String select;
+    String theDance;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,8 +41,34 @@ public class showFiles extends AppCompatActivity implements AdapterView.OnItemCl
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_files);
 
+        findViewById(R.id.openProject).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Intent intent = new Intent(showFiles.this, viewProject.class);
+                final ParseQuery<ParseObject> parseQuery = ParseQuery.getQuery("dances");
+                parseQuery.whereEqualTo("nameOfDance", select);
+                parseQuery.findInBackground(new FindCallback<ParseObject>() {
+                    @Override
+                    public void done(List<ParseObject> objects, ParseException e) {
+                        if(e == null){
+                            for (ParseObject item : objects){
+                                theDance = item.get("nameOfDance").toString();
+                                intent.putExtra("danceAlgo", theDance);
+                                startActivity(intent);
+                            }
+                        }else {
+                            Toast.makeText(showFiles.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
+
+            }
+        });
+
 
         dances = new ArrayList<>();
+        projectInfo = new ArrayList<>();
             displayText = findViewById(R.id.displayText);
             listView = findViewById(R.id.list);
             findItems();
@@ -49,12 +80,16 @@ public class showFiles extends AppCompatActivity implements AdapterView.OnItemCl
             listView.setAdapter(arrayAdapter);
             listView.setOnItemClickListener(this);
 
-
     }
+
+
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
         displayText.setText(dances.get(position).toString());
+        select = dances.get(position).toString();
     }
+
+
     public void findItems(){
         final ParseQuery<ParseObject> parseQuery = ParseQuery.getQuery("dances");
         parseQuery.findInBackground(new FindCallback<ParseObject>() {
--- app/src/main/java/com/nerya/rollup/viewProject.java
+++ app/src/main/java/com/nerya/rollup/viewProject.java
@@ -2,13 +2,77 @@ package com.nerya.rollup;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class viewProject extends AppCompatActivity {
+
+    TextView view, viewa, viewb, viewc, viewd, viewe, viewf, viewg, viewh, viewi, frameDisplay;
+    ArrayList dance;
+    String danceName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_project);
+
+        view = findViewById(R.id.dad);
+        viewa = findViewById(R.id.dada);
+        viewb = findViewById(R.id.dadb);
+        viewc = findViewById(R.id.dadc);
+        viewd = findViewById(R.id.dadd);
+        viewe = findViewById(R.id.dade);
+        viewf = findViewById(R.id.dadf);
+        viewg = findViewById(R.id.dadg);
+        viewh = findViewById(R.id.dadh);
+        viewi = findViewById(R.id.dadi);
+
+        dance = new ArrayList<>();
+        danceName = getIntent().getStringExtra("danceAlgo");
+        Log.i("dance", danceName + "");
+        ParseQuery<ParseObject> query = ParseQuery.getQuery("dances");
+        query.whereEqualTo("nameOfDance", danceName + "");
+        query.findInBackground(new FindCallback<ParseObject>() {
+            @Override
+            public void done(List<ParseObject> objects, ParseException e) {
+                if(e == null){
+                    if(objects.size() > 0){
+                        for (ParseObject list : objects){
+                            list.get("dance");
+                            dance = (ArrayList)list.get("dance");
+                            Log.i("dance", dance.toString());
+                        }
+                    }else {
+                        Toast.makeText(viewProject.this, "no objects", Toast.LENGTH_SHORT).show();
+                    }
+                }else {
+                    Toast.makeText(viewProject.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        try {
+            getSupportActionBar().hide();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        setXY(view.getId(), 1000, 1000);
+    }
+
+    public void setXY(int id, float x, float y){
+        findViewById(id).setTranslationX(x);
+        findViewById(id).setTranslationY(y);
     }
 }
